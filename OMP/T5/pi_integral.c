80aff336cb8a6fb302c7daa6aad75b0a252a4d8d
bryanblancas <orionmunecaycanica@gmail.com>
2018-10-25 12:03:03 -0500

    final

--- OMP/T5/pi_integral.c
+++ OMP/T5/pi_integral.c
@@ -6,7 +6,7 @@ int main(int argc, char const *argv[]){
 
   	//variables del programa
 	double area, pi, x;		
-	int i, n; 				//n número de rectángulos
+	int i, n, thrs; 				//n número de rectángulos
 
 	//validación de argumentos
 	if(argc != 3){
@@ -19,12 +19,17 @@ int main(int argc, char const *argv[]){
 	omp_set_num_threads(atoi(argv[2]));
 
 	area = 0.0;
-	#pragma omp parallel for private(x) reduction(+:area)
+	#pragma omp parallel 
+	{
+		if(omp_get_thread_num()==0)
+			printf("There are %d threads!\n",omp_get_num_threads());
+
+		#pragma omp for private(x) reduction(+:area) 
 		for(i=0 ; i<n ; i++){
 			x = (i + 0.5)/n;			//encontrar el x actual (rectángulo central)
 			area += 4.0/(1.0 + (x*x)); 	//área en x
 		}
-	
+	}
 	pi = area/n;	//pi = área entre todos las divisiones
 	printf("Pi: %.5lf\n",pi);
  	
